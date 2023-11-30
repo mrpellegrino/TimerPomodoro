d110e43a41fffff9d861e0826a291b03b4e37e3e
mrpellegrino <dayvson.pellegrinorodrigues@gmail.com>
2023-11-30 16:12:48 -0300

    chore: study about styled components

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from "./components/Button";
+
+export function App() {
+
+
+  return (
+    <>
+     <Button variant='primary' />
+     <Button variant='secondary'/>
+     <Button variant='danger'/>
+     <Button variant='success'/>
+     <Button />
+
+    </>
+  )
+}
+
+
